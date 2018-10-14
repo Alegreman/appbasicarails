22294bdf15de4e2a80397b54510dd0f4f96bffcb
jorgealegria <jobaalegriaji@ittepic.edu.mx>
2018-10-13 19:21:32 -0600

    agregué las vistas a mostrar en la pagina de bienvenida y en la ruta de saludo

--- appbasica/config/routes.rb
+++ appbasica/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'ruta/saludo'
   get 'welcome/index'
+  root 'ruta#saludo'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
